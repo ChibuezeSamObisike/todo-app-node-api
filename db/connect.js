3c382ed290465c49ef875d2a5914fd6c3dbd3794
mac <mac@macs-MacBook-Pro-2.local>
2023-12-13 09:43:00 -0800

    chore: push to prod

--- db/connect.js
+++ db/connect.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionString = '';
+const connectionString = "";
 
 const connectDB = (url) => {
-    //Setting the options because of the Deprecated version I am using
-    return mongoose.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-    });
+  //Setting the options because of the Deprecated version I am using so I am testing my new macbook commands, lol
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
 };
 
 module.exports = connectDB;
--- errors/custom-error.js
+++ errors/custom-error.js
@@ -1,12 +1,12 @@
 class CustomAPIError extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.statusCode = statusCode;
-    }
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
 }
 
 const createCustomError = (msg, statusCode) => {
-    return new CustomAPIError(msg, statusCode);
+  return new CustomAPIError(msg, statusCode);
 };
 
 module.exports = { createCustomError, CustomAPIError };
